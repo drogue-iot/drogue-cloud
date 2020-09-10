5b6e7e150ec873009826182fb5ea53f6cf2618f6
Jens Reimann <jreimann@redhat.com>
2020-09-10 11:52:46 +0200

    Do some work

---
+++ http-endpoint/src/http.rs
@@ -0,0 +1,88 @@
+use actix_web::{get, middleware, web, App, HttpResponse, HttpServer, Responder};
+use log;
+use serde::{Deserialize, Serialize};
+
+use actix_web::error::PayloadError;
+use actix_web::web::Bytes;
+use anyhow::Context;
+use cloudevents::{EventBuilder, EventBuilderV10};
+use futures::StreamExt;
+use futures_core::Stream;
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Publish {
+    pub channel: String,
+}
+
+#[derive(Clone, Copy, Debug, Serialize, Deserialize)]
+pub enum Outcome {
+    Accepted,
+    Rejected,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct PublishResponse {
+    pub outcome: Outcome,
+}
+
+#[derive(Clone, Debug)]
+pub struct HttpEndpoint {
+    client: reqwest::Client,
+    sink: String,
+}
+
+impl HttpEndpoint {
+    pub fn new() -> anyhow::Result<Self> {
+        let sink = std::env::var("K_SINK").context("Missing variable 'K_SINK'")?;
+
+        Ok(HttpEndpoint {
+            client: reqwest::ClientBuilder::new().build()?,
+            sink,
+        })
+    }
+
+    pub async fn publish<S>(
+        &self,
+        publish: Publish,
+        mut body: S,
+    ) -> Result<PublishResponse, actix_web::Error>
+    where
+        S: Stream<Item = Result<Bytes, PayloadError>> + Unpin,
+    {
+        let mut bytes = web::BytesMut::new();
+        while let Some(item) = body.next().await {
+            bytes.extend_from_slice(&item?);
+        }
+        let bytes = bytes.freeze();
+
+        let event = EventBuilderV10::new()
+            .source("https://dentrassi.de/iot")
+            .subject(&publish.channel)
+            .data("application/octet-stream", bytes.to_vec())
+            .ty("de.dentrassi.iot.message");
+
+        let event = event
+            .build()
+            .map_err(actix_web::error::ErrorInternalServerError)?;
+
+        let response =
+            cloudevents_sdk_reqwest::event_to_request(event, self.client.post(&self.sink))
+                // If i can't build the request, fail with internal server error
+                .map_err(actix_web::error::ErrorInternalServerError)?
+                .send()
+                .await
+                // If something went wrong when sending the event, fail with internal server error
+                .map_err(actix_web::error::ErrorInternalServerError)?;
+
+        log::info!("Publish result: {:?}", response);
+
+        match response.status().is_success() {
+            true => Ok(PublishResponse {
+                outcome: Outcome::Accepted,
+            }),
+            false => Ok(PublishResponse {
+                outcome: Outcome::Rejected,
+            }),
+        }
+    }
+}
--- http-endpoint/src/main.rs
+++ http-endpoint/src/main.rs
@@ -1,7 +1,12 @@
+mod http;
+
+use crate::http::{HttpEndpoint, Outcome, Publish, PublishResponse};
+use actix_web::dev::HttpResponseBuilder;
+use actix_web::http::StatusCode;
 use actix_web::{get, middleware, web, App, HttpResponse, HttpServer, Responder};
 use log;
 
-pub struct PublishResponse {}
+use futures_util::StreamExt;
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -9,25 +14,50 @@ async fn index() -> impl Responder {
 }
 
 #[get("/publish/{channel}")]
-async fn publish(web::Path(channel): web::Path<String>) -> impl Responder {
+async fn publish(
+    endpoint: web::Data<HttpEndpoint>,
+    web::Path(channel): web::Path<String>,
+    mut body: web::Payload,
+) -> Result<HttpResponse, actix_web::Error> {
     log::info!("Published to '{}'", channel);
-    HttpResponse::Ok()
+
+    match endpoint.publish(Publish { channel }, body).await {
+        // ok, and accepted
+        Ok(PublishResponse {
+            outcome: Outcome::Accepted,
+        }) => Ok(HttpResponse::Accepted().finish()),
+
+        // ok, but rejected
+        Ok(PublishResponse {
+            outcome: Outcome::Rejected,
+        }) => Ok(HttpResponse::NotAcceptable().finish()),
+
+        // internal error
+        Err(err) => Ok(HttpResponse::InternalServerError()
+            .content_type("text/plain")
+            .body(err.to_string())),
+    }
 }
 
 const GLOBAL_MAX_JSON_PAYLOAD_SIZE: usize = 64 * 1024;
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> anyhow::Result<()> {
     env_logger::init();
 
-    HttpServer::new(|| {
+    let endpoint = HttpEndpoint::new()?;
+
+    HttpServer::new(move || {
         App::new()
             .wrap(middleware::Logger::default())
             .data(web::JsonConfig::default().limit(GLOBAL_MAX_JSON_PAYLOAD_SIZE))
+            .data(endpoint.clone())
             .service(index)
             .service(publish)
     })
     .bind("127.0.0.1:8080")?
     .run()
-    .await
+    .await?;
+
+    Ok(())
 }
