7589320919d063224735b017f62425943985389c
Jens Reimann <jreimann@redhat.com>
2020-11-06 17:42:13 +0100

    Add model id to the publishing loop

--- hack/publish_loop.sh
+++ hack/publish_loop.sh
@@ -4,10 +4,11 @@
 # Publish a "temperature" every 5 seconds
 #
 
-DEVICE="dev1"
+DEVICE="my:dev1"
 CHANNEL="foo"
+MODEL_ID="vorto.private.ctron:DeviceOne:1.0.0"
 
 while true; do
-  http -v POST "https://http-endpoint-drogue-iot.apps.wonderful.iot-playground.org/publish/${DEVICE}/${CHANNEL}" temp:="$(printf "%f" "$(echo "s ( $(date +%s ) * 0.02 ) * 10 + 10" | bc -l)")"
+  http -v POST "https://http-endpoint-drogue-iot.apps.wonderful.iot-playground.org/publish/${DEVICE}/${CHANNEL}" "model_id==$MODEL_ID" temp:="$(printf "%f" "$(echo "s ( $(date +%s ) * 0.02 ) * 10 + 10" | bc -l)")"
   sleep 5
 done
