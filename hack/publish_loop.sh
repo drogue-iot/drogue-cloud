351579084c185b74037c1971e5e636a4b3cea3e4
Jens Reimann <jreimann@redhat.com>
2020-11-09 12:04:24 +0100

    Add readme for digital twin

--- hack/publish_loop.sh
+++ hack/publish_loop.sh
@@ -4,11 +4,12 @@
 # Publish a "temperature" every 5 seconds
 #
 
-DEVICE="my:dev1"
-CHANNEL="foo"
-MODEL_ID="vorto.private.ctron:DeviceOne:1.0.0"
+: "${ENDPOINT:="https://http-endpoint-drogue-iot.apps.wonderful.iot-playground.org"}"
+: "${DEVICE:="my:dev1"}"
+: "${CHANNEL:="foo"}"
+: "${MODEL_ID:="io.drogue.demo:FirstTestDevice:1.0.0"}"
 
 while true; do
-  http -v POST "https://http-endpoint-drogue-iot.apps.wonderful.iot-playground.org/publish/${DEVICE}/${CHANNEL}" "model_id==$MODEL_ID" temp:="$(printf "%f" "$(echo "s ( $(date +%s ) * 0.02 ) * 10 + 10" | bc -l)")"
+  http -v POST "${ENDPOINT}/publish/${DEVICE}/${CHANNEL}" "model_id==$MODEL_ID" temp:="$(printf "%f" "$(echo "s ( $(date +%s ) * 0.02 ) * 10 + 10" | bc -l)")"
   sleep 5
 done
