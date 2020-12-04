cb5b0115bb5c3bc1994c4b985060353a0c3d8313
Jens Reimann <jreimann@redhat.com>
2020-12-04 17:54:21 +0100

    Add some example commands

--- hack/status.sh
+++ hack/status.sh
@@ -76,6 +76,10 @@ echo
 echo "URL:"
 echo "    ${MGMT_URL}"
 echo
+echo "  Create:  http POST ${MGMT_URL}/api/v1/devices device_id=device_id password=foobar"
+echo "  Read:    http GET ${MGMT_URL}/api/v1/devices/device_id"
+echo "  Delete:  http DELETE ${MGMT_URL}/api/v1/devices/device_id"
+echo
 echo "Publish data:"
 echo "----------------"
 echo
