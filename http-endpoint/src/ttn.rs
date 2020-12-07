83813280d2d938821c638d5f2d87d483e8a191fc
Jens Reimann <jreimann@redhat.com>
2020-12-07 14:02:29 +0100

    Implement function port mapping

--- http-endpoint/src/ttn.rs
+++ http-endpoint/src/ttn.rs
@@ -13,7 +13,7 @@ use crate::PublishOptions;
 pub async fn publish(
     endpoint: web::Data<DownstreamSender>,
     web::Query(opts): web::Query<PublishOptions>,
-    device_properties: Option<DeviceProperties>,
+    props: Option<DeviceProperties>,
     body: web::Bytes,
 ) -> Result<HttpResponse, HttpEndpointError> {
     let uplink: ttn::Uplink = serde_json::from_slice(&body).map_err(|err| {
@@ -23,14 +23,27 @@ pub async fn publish(
         }
     })?;
 
-    log::info!("Device properties: {:?}", device_properties);
+    log::info!("Device properties: {:?}", props);
+
+    // eval model_id from query and function port mapping
+
+    let fport = uplink.port.to_string();
+    let model_id = opts.model_id.or_else(|| {
+        props.as_ref().map(|props| &props.0).and_then(|props| {
+            props["lorawan"]["ports"][fport]["model_id"]
+                .as_str()
+                .map(|str| str.to_string())
+        })
+    });
+
+    log::info!("Model ID: {:?}", model_id);
 
     endpoint
         .publish_http(
             Publish {
                 channel: uplink.port.to_string(),
                 device_id: uplink.dev_id,
-                model_id: opts.model_id,
+                model_id,
                 ..Default::default()
             },
             body,
