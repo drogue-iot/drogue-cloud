906cbf91e1344ff998d49e3a38363d2e000a855a
Dejan Bosanac <dejan@sensatic.net>
2021-01-25 13:48:22 +0100

    Use macros to avoid duplicate code between MQTT v3 and v5 logic implementations

--- mqtt-endpoint/src/mqtt.rs
+++ mqtt-endpoint/src/mqtt.rs
@@ -20,48 +20,58 @@ use drogue_cloud_endpoint_common::{
 use std::fmt::Debug;
 use tokio::sync::mpsc;
 
+macro_rules! connect {
+    ($connect:expr, $app: expr) => {{
+        log::info!("new connection: {:?}", $connect);
+        let device_id = $connect.packet().client_id.to_string();
+        match $app
+            .authenticate(
+                &$connect.packet().username,
+                &$connect.packet().password,
+                &$connect.packet().client_id,
+            )
+            .await?
+        {
+            auth::Outcome::Pass(_) => {
+                let (tx, mut rx) = mpsc::channel(32);
+
+                let session =
+                    Session::new($app.downstream, device_id.clone(), $app.devices.clone(), tx);
+
+                let sink = $connect.sink().clone();
+                ntex::rt::spawn(async move {
+                    while let Some(cmd) = rx.recv().await {
+                        match sink
+                            .publish(ByteString::from_static("cmd"), Bytes::from(cmd))
+                            .send_at_least_once()
+                            .await
+                        {
+                            Ok(_) => log::debug!(
+                                "Command sent to device subscription {:?}",
+                                device_id.clone()
+                            ),
+                            Err(e) => log::error!(
+                                "Failed to send a command to device subscription {:?}",
+                                e
+                            ),
+                        }
+                    }
+                });
+
+                Ok(session)
+            }
+            auth::Outcome::Fail => Err("Failed"),
+        }
+    }};
+}
+
 pub async fn connect_v3<Io>(
     connect: v3::Connect<Io>,
     app: App,
 ) -> Result<v3::ConnectAck<Io, Session>, ServerError> {
-    log::info!("new connection: {:?}", connect);
-    let device_id = connect.packet().client_id.to_string();
-
-    match app
-        .authenticate(
-            &connect.packet().username,
-            &connect.packet().password,
-            &connect.packet().client_id,
-        )
-        .await?
-    {
-        auth::Outcome::Pass(_) => {
-            let (tx, mut rx) = mpsc::channel(32);
-
-            let session = Session::new(app.downstream, device_id.clone(), app.devices.clone(), tx);
-
-            let sink = connect.sink().clone();
-            ntex::rt::spawn(async move {
-                while let Some(cmd) = rx.recv().await {
-                    match sink
-                        .publish(ByteString::from_static("cmd"), Bytes::from(cmd))
-                        .send_at_least_once()
-                        .await
-                    {
-                        Ok(_) => log::debug!(
-                            "Command sent to device subscription {:?}",
-                            device_id.clone()
-                        ),
-                        Err(e) => {
-                            log::error!("Failed to send a command to device subscription {:?}", e)
-                        }
-                    }
-                }
-            });
-
-            Ok(connect.ack(session, false))
-        }
-        auth::Outcome::Fail => Ok(connect.bad_username_or_pwd()),
+    match connect!(connect, app) {
+        Ok(session) => Ok(connect.ack(session, false)),
+        Err(_) => Ok(connect.bad_username_or_pwd()),
     }
 }
 
@@ -69,110 +79,41 @@ pub async fn connect_v5<Io>(
     connect: v5::Connect<Io>,
     app: App,
 ) -> Result<v5::ConnectAck<Io, Session>, ServerError> {
-    log::info!("new connection: {:?}", connect);
-    let device_id = connect.packet().client_id.to_string();
-
-    match app
-        .authenticate(
-            &connect.packet().username,
-            &connect.packet().password,
-            &connect.packet().client_id,
-        )
-        .await?
-    {
-        auth::Outcome::Pass(_) => {
-            let (tx, mut rx) = mpsc::channel(32);
-
-            let session = Session::new(app.downstream, device_id.clone(), app.devices.clone(), tx);
-
-            let sink = connect.sink().clone();
-            ntex::rt::spawn(async move {
-                while let Some(cmd) = rx.recv().await {
-                    match sink
-                        .publish(ByteString::from_static("cmd"), Bytes::from(cmd))
-                        .send_at_least_once()
-                        .await
-                    {
-                        Ok(_) => log::debug!(
-                            "Command sent to device subscription {:?}",
-                            device_id.clone()
-                        ),
-                        Err(e) => {
-                            log::error!("Failed to send a command to device subscription {:?}", e)
-                        }
-                    }
-                }
-            });
-
-            Ok(connect.ack(session).with(|ack| {
-                ack.wildcard_subscription_available = Some(false);
-            }))
-        }
-        auth::Outcome::Fail => Ok(connect.failed(ConnectAckReason::BadUserNameOrPassword)),
+    match connect!(connect, app) {
+        Ok(session) => Ok(connect.ack(session).with(|ack| {
+            ack.wildcard_subscription_available = Some(false);
+        })),
+        Err(_) => Ok(connect.failed(ConnectAckReason::BadUserNameOrPassword)),
     }
 }
 
-pub async fn publish_v5(
-    session: v5::Session<Session>,
-    publish: v5::Publish,
-) -> Result<v5::PublishAck, ServerError> {
-    log::info!("incoming publish: {:?} - {:?}", publish, publish.packet());
+macro_rules! publish {
+    ($session: expr, $publish:expr) => {{
+        log::info!(
+            "incoming publish: {:?} -> {:?} / {:?}",
+            $publish.id(),
+            $publish.topic(),
+            $publish.packet(),
+        );
+        let channel = $publish.topic().path();
 
-    let channel = publish.topic().path();
-
-    match session
-        .state()
-        .sender
-        .publish(
+        $session.state().sender.publish(
             Publish {
                 channel: channel.into(),
-                device_id: session.device_id.clone(),
+                device_id: $session.device_id.clone(),
                 model_id: None,
                 ..Default::default()
             },
-            publish.payload(),
+            $publish.payload(),
         )
-        .await
-    {
-        Ok(PublishResponse {
-            outcome: Outcome::Accepted,
-        }) => Ok(publish.ack()),
-        Ok(PublishResponse {
-            outcome: Outcome::Rejected,
-        }) => Ok(publish
-            .ack()
-            .reason_code(PublishAckReason::UnspecifiedError)),
-        Err(e) => Err(ServerError { msg: e.to_string() }),
-    }
+    }};
 }
 
 pub async fn publish_v3(
     session: v3::Session<Session>,
     publish: v3::Publish,
 ) -> Result<(), ServerError> {
-    log::info!(
-        "incoming publish: {:?} -> {:?} / {:?}",
-        publish.id(),
-        publish.topic(),
-        publish.packet(),
-    );
-
-    let channel = publish.topic().path();
-
-    match session
-        .state()
-        .sender
-        .publish(
-            Publish {
-                channel: channel.into(),
-                device_id: session.device_id.clone(),
-                model_id: None,
-                ..Default::default()
-            },
-            publish.payload(),
-        )
-        .await
-    {
+    match publish!(session, publish).await {
         Ok(PublishResponse {
             outcome: Outcome::Accepted,
         }) => Ok(()),
@@ -188,6 +129,44 @@ pub async fn publish_v3(
     }
 }
 
+pub async fn publish_v5(
+    session: v5::Session<Session>,
+    publish: v5::Publish,
+) -> Result<v5::PublishAck, ServerError> {
+    match publish!(session, publish).await {
+        Ok(PublishResponse {
+            outcome: Outcome::Accepted,
+        }) => Ok(publish.ack()),
+        Ok(PublishResponse {
+            outcome: Outcome::Rejected,
+        }) => Ok(publish
+            .ack()
+            .reason_code(PublishAckReason::UnspecifiedError)),
+        Err(e) => Err(ServerError { msg: e.to_string() }),
+    }
+}
+
+macro_rules! subscribe {
+    ($s: expr, $session: expr) => {{
+        $s.iter_mut().for_each(|mut sub| {
+            sub.subscribe(QoS::AtLeastOnce);
+        });
+
+        let mut devices = $session.state().devices.lock().unwrap();
+        devices.insert(
+            $session.state().device_id.clone(),
+            $session.state().tx.clone(),
+        );
+
+        log::debug!(
+            "Device '{:?}' subscribed to receive commands",
+            $session.state().device_id.clone()
+        );
+
+        Ok($s.ack())
+    }};
+}
+
 pub async fn control_v3(
     session: v3::Session<Session>,
     control: v3::ControlMessage,
@@ -195,24 +174,7 @@ pub async fn control_v3(
     match control {
         v3::ControlMessage::Ping(p) => Ok(p.ack()),
         v3::ControlMessage::Disconnect(d) => Ok(d.ack()),
-        v3::ControlMessage::Subscribe(mut s) => {
-            s.iter_mut().for_each(|mut sub| {
-                sub.subscribe(QoS::AtLeastOnce);
-            });
-
-            let mut devices = session.state().devices.lock().unwrap();
-            devices.insert(
-                session.state().device_id.clone(),
-                session.state().tx.clone(),
-            );
-
-            log::debug!(
-                "Device '{:?}' subscribed to receive commands",
-                session.state().device_id.clone()
-            );
-
-            Ok(s.ack())
-        }
+        v3::ControlMessage::Subscribe(mut s) => subscribe!(s, session),
         v3::ControlMessage::Unsubscribe(u) => Ok(u.ack()),
         v3::ControlMessage::Closed(c) => Ok(c.ack()),
     }
@@ -222,32 +184,13 @@ pub async fn control_v5<E: Debug>(
     session: v5::Session<Session>,
     control: v5::ControlMessage<E>,
 ) -> Result<v5::ControlResult, ServerError> {
-    // log::info!("Control message: {:?}", control);
-
     match control {
         v5::ControlMessage::Auth(a) => Ok(a.ack(Auth::default())),
         v5::ControlMessage::Error(e) => Ok(e.ack(DisconnectReasonCode::UnspecifiedError)),
         v5::ControlMessage::ProtocolError(pe) => Ok(pe.ack()),
         v5::ControlMessage::Ping(p) => Ok(p.ack()),
         v5::ControlMessage::Disconnect(d) => Ok(d.ack()),
-        v5::ControlMessage::Subscribe(mut s) => {
-            s.iter_mut().for_each(|mut sub| {
-                sub.subscribe(QoS::AtLeastOnce);
-            });
-
-            let mut devices = session.state().devices.lock().unwrap();
-            devices.insert(
-                session.state().device_id.clone(),
-                session.state().tx.clone(),
-            );
-
-            log::debug!(
-                "Device '{:?}' subscribed to receive commands",
-                session.state().device_id.clone()
-            );
-
-            Ok(s.ack())
-        }
+        v5::ControlMessage::Subscribe(mut s) => subscribe!(s, session),
         v5::ControlMessage::Unsubscribe(u) => Ok(u.ack()),
         v5::ControlMessage::Closed(c) => Ok(c.ack()),
     }
