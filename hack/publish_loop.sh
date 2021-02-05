3e8bed9584e6bae2042f22b08b4e5c12a04a8a69
Jens Reimann <jreimann@redhat.com>
2021-02-05 13:55:11 +0100

    fix up publish_loop script

--- hack/publish_loop.sh
+++ hack/publish_loop.sh
@@ -5,11 +5,13 @@
 #
 
 : "${ENDPOINT:="https://http-endpoint-drogue-iot.apps.my.cluster"}"
-: "${DEVICE_ID:="my:dev1"}"
 : "${CHANNEL:="foo"}"
 : "${MODEL_ID:="io.drogue.demo:FirstTestDevice:1.0.0"}"
+: "${APP:=app_id}"
+: "${DEVICE:=device_id}"
+: "${PASS:=foobar}"
 
 while true; do
-  http -v POST "${ENDPOINT}/v1/${CHANNEL}" "model_id==$MODEL_ID" temp:="$(printf "%f" "$(echo "s ( $(date +%s ) * 0.02 ) * 10 + 10" | bc -l)")"
+  http -v --auth "${DEVICE}@${APP}:${PASS}" ${CERT:+--verify $CERT} POST "${ENDPOINT}/v1/${CHANNEL}" "model_id==$MODEL_ID" temp:="$(printf "%f" "$(echo "s ( $(date +%s ) * 0.02 ) * 10 + 10" | bc -l)")"
   sleep 5
 done
