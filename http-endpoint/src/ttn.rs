eff29a2ed1922667bb8c71bf16dc2b24426e8da5
Jens Reimann <jreimann@redhat.com>
2021-02-17 10:17:29 +0100

    Fix some issues with the TTN endpoint

--- http-endpoint/src/ttn.rs
+++ http-endpoint/src/ttn.rs
@@ -1,6 +1,6 @@
 use crate::command::CommandWait;
 use crate::downstream::HttpCommandSender;
-use actix_web::{put, web, HttpResponse};
+use actix_web::{post, web, HttpResponse};
 use drogue_cloud_endpoint_common::{
     auth::DeviceAuthenticator,
     downstream::{DownstreamSender, Publish},
@@ -8,6 +8,7 @@ use drogue_cloud_endpoint_common::{
     x509::ClientCertificateChain,
 };
 use drogue_cloud_service_api::auth;
+use drogue_cloud_service_api::management::Device;
 use drogue_ttn::http as ttn;
 use serde::Deserialize;
 
@@ -20,7 +21,7 @@ pub struct PublishOptions {
     pub ttd: Option<u64>,
 }
 
-#[put("")]
+#[post("")]
 pub async fn publish(
     sender: web::Data<DownstreamSender>,
     auth: web::Data<DeviceAuthenticator>,
@@ -61,12 +62,7 @@ pub async fn publish(
     );
 
     // eval model_id from query and function port mapping
-    let model_id = opts.model_id.or_else(|| {
-        let fport = uplink.port.to_string();
-        device.spec["lorawan"]["ports"][fport]["model_id"]
-            .as_str()
-            .map(|str| str.to_string())
-    });
+    let model_id = eval_model_id(opts.model_id.as_ref().cloned(), &device, &uplink);
 
     let device_id = uplink.dev_id;
 
@@ -86,4 +82,115 @@ pub async fn publish(
             body,
         )
         .await
+}
+
+fn eval_model_id(
+    model_id: Option<String>,
+    device: &Device,
+    uplink: &ttn::Uplink,
+) -> Option<String> {
+    model_id.or_else(|| {
+        let fport = uplink.port.to_string();
+        device.spec.get("lorawan").and_then(|spec| {
+            spec["ports"][fport]["model_id"]
+                .as_str()
+                .map(|str| str.to_string())
+        })
+    })
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+    use chrono::Utc;
+    use drogue_ttn::http::Metadata;
+    use serde_json::{json, Map, Value};
+
+    #[test]
+    fn test_model_mapping() {
+        let lorawan_spec = json!({
+            "ports": {
+             "1": { "model_id": "mod1",},
+             "5": {"model_id": "mod5",},
+            }
+        });
+
+        let device = device(Some(lorawan_spec));
+        let uplink = default_uplink(5);
+
+        let model_id = eval_model_id(None, &device, &uplink);
+
+        assert_eq!(model_id, Some(String::from("mod5")));
+    }
+
+    #[test]
+    fn test_model_no_mapping_1() {
+        let device = device(None);
+        let uplink = default_uplink(5);
+
+        let model_id = eval_model_id(None, &device, &uplink);
+
+        assert_eq!(model_id, None);
+    }
+
+    #[test]
+    fn test_model_no_mapping_2() {
+        let device = device(Some(json!({
+            "ports": { "1": {"model_id": "mod1"}}
+        })));
+        let uplink = default_uplink(5);
+
+        let model_id = eval_model_id(None, &device, &uplink);
+
+        assert_eq!(model_id, None);
+    }
+
+    #[test]
+    fn test_model_no_mapping_3() {
+        let device = device(Some(json!({
+            "ports": { "1": {"no_model_id": "mod1"}}
+        })));
+        let uplink = default_uplink(5);
+
+        let model_id = eval_model_id(None, &device, &uplink);
+
+        assert_eq!(model_id, None);
+    }
+
+    fn device(lorawan_spec: Option<Value>) -> Device {
+        let mut spec = Map::new();
+        if let Some(lorawan_spec) = lorawan_spec {
+            spec.insert("lorawan".into(), lorawan_spec);
+        }
+        Device {
+            metadata: Default::default(),
+            spec,
+            status: Default::default(),
+        }
+    }
+
+    fn default_uplink(port: u16) -> ttn::Uplink {
+        ttn::Uplink {
+            app_id: "".to_string(),
+            dev_id: "".to_string(),
+            hardware_serial: "".to_string(),
+            port,
+            counter: 0,
+            is_retry: false,
+            confirmed: false,
+            payload_raw: vec![],
+            metadata: Metadata {
+                time: Utc::now(),
+                frequency: 0.0,
+                modulation: "".to_string(),
+                data_rate: None,
+                bit_rate: None,
+                coding_rate: "".to_string(),
+                coordinates: None,
+                gateways: vec![],
+            },
+            downlink_url: None,
+        }
+    }
 }
