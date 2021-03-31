a16c2d576b62f5fa7afa0c093e3699606b297115
Jens Reimann <jreimann@redhat.com>
2021-03-31 16:22:49 +0200

    Push with retry, working around rate limiting issues

---
+++ scripts/retry.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+: "${MAX_RETRY:=500}"
+: "${WAIT:=10}"
+
+: "${CONTAINER:=docker}"
+RETRY=0
+
+echo "Run with retry: $CONTAINER " "$@"
+
+while (( RETRY < MAX_RETRY )); do
+  echo "Pushing image - attempt $RETRY of $MAX_RETRY"
+  (( RETRY -- ))
+  "$CONTAINER" "$@" && exit 0
+  echo "Failed to execute ... sleeping: $WAIT seconds"
+  sleep "$WAIT"
+done
