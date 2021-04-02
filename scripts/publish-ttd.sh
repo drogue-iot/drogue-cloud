815b7a66f2b22c532dd4a00a9fe0515bf61c1966
Dejan Bosanac <dejan@sensatic.net>
2021-04-02 15:56:28 +0200

    Add example simulator for the ttn endppint

---
+++ scripts/publish-ttd.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+#
+# Publish a "temperature" every 5 seconds
+#
+
+: "${ENDPOINT:="https://http.sandbox.drogue.cloud"}"
+: "${APP:=lora-app}"
+: "${DEVICE:=ttn-service}"
+: "${PASS:=foobar}"
+
+while true; do
+    data=`echo -n "{\"temp\":$((20 + $RANDOM % 5))}" | base64`
+
+    payload="{
+        \"app_id\": \"kubecon\",
+        \"counter\": 0,
+        \"dev_id\": \"kubecon-device\",
+        \"downlink_url\": \"https://integrations.thethingsnetwork.org/ttn-eu/api/v2/down/xxx/xxx?key=xxx\",
+        \"hardware_serial\": \"000A22A981D142F0\",
+        \"metadata\": {
+            \"time\": \"2021-04-01T11:30:52.016876672Z\"
+        },
+        \"payload_raw\": \"$data\",
+        \"port\": 1
+    }"
+   echo $payload | http --verbose --auth "${DEVICE}@${APP}:${PASS}" ${CERT:+--verify $CERT} POST ${ENDPOINT}/ttn
+   sleep 5
+ done
