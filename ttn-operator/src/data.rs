e4ce0255dceec4f103b2cfc32ca7d0e85a02b258
Jens Reimann <jreimann@redhat.com>
2021-04-20 08:54:56 +0200

    More work on reconciling

--- ttn-operator/src/data.rs
+++ ttn-operator/src/data.rs
@@ -1,6 +1,35 @@
-use drogue_client::{Dialect, Section};
+use crate::error::ReconcileError;
+use crate::ttn;
+use drogue_client::{dialect, Dialect, Section};
 use serde::{Deserialize, Serialize};
 use url::Url;
+
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq)]
+#[serde(untagged)]
+pub enum RegionOrUrl {
+    Url(Url),
+    Region(String),
+}
+
+impl RegionOrUrl {
+    pub fn url(&self) -> Result<Url, url::ParseError> {
+        match self {
+            Self::Url(url) => Ok(url.clone()),
+            Self::Region(region) => {
+                Url::parse(&format!("https://{}.cloud.thethings.network", region))
+            }
+        }
+    }
+}
+
+#[derive(Clone, Debug, Default, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TtnReconcileStatus {
+    pub observed_generation: u64,
+    pub state: String,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub reason: Option<String>,
+}
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -8,7 +37,7 @@ pub struct TtnAppSpec {
     pub api: TtnAppApi,
 }
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
 pub struct TtnAppApi {
     pub api_key: String,
@@ -19,56 +48,51 @@ pub struct TtnAppApi {
     pub id: Option<String>,
 }
 
-#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq)]
-#[serde(untagged)]
-pub enum RegionOrUrl {
-    Url(Url),
-    Region(String),
-}
-
-impl RegionOrUrl {
-    pub fn url(&self) -> Result<Url, anyhow::Error> {
-        match self {
-            Self::Url(url) => Ok(url.clone()),
-            Self::Region(region) => Ok(Url::parse(&format!(
-                "https://{}.cloud.thethings.network",
-                region
-            ))?),
-        }
+impl TtnAppApi {
+    pub fn to_context(&self) -> Result<ttn::Context, ReconcileError> {
+        Ok(ttn::Context {
+            api_key: self.api_key.clone(),
+            url: self.region.url().map_err(|err| {
+                ReconcileError::Permanent(format!("Failed to parse URL: {}", err))
+            })?,
+        })
     }
 }
 
 #[derive(Clone, Debug, Default, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct TtnAppStatus {
-    pub observed_generation: u64,
-    pub state: String,
-    #[serde(default, skip_serializing_if = "Option::is_none")]
-    pub reason: Option<String>,
-
-    #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[serde(flatten)]
+    pub reconcile: TtnReconcileStatus,
     pub app_id: Option<String>,
 }
 
-impl Dialect for TtnAppSpec {
-    fn key() -> &'static str {
-        "ttn"
-    }
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct TtnDeviceSpec {
+    pub dev_eui: String,
+    pub app_eui: String,
+    pub app_key: String,
 
-    fn section() -> Section {
-        Section::Spec
-    }
+    pub lorawan_version: String,
+    pub lorawan_phy_version: String,
+    #[serde(default)]
+    pub supports_class_b: bool,
+    #[serde(default)]
+    pub supports_class_c: bool,
+    pub frequency_plan: String,
 }
 
-impl Dialect for TtnAppStatus {
-    fn key() -> &'static str {
-        "ttn"
-    }
-
-    fn section() -> Section {
-        Section::Status
-    }
+#[derive(Clone, Debug, Default, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TtnDeviceStatus {
+    pub reconcile: TtnReconcileStatus,
 }
+
+dialect!(TtnDeviceSpec[Section::Spec => "ttn"]);
+dialect!(TtnDeviceStatus[Section::Status => "ttn"]);
+
+dialect!(TtnAppSpec[Section::Spec => "ttn"]);
+dialect!(TtnAppStatus[Section::Status => "ttn"]);
 
 #[cfg(test)]
 mod test {
--- ttn-operator/src/ttn.rs
+++ ttn-operator/src/ttn.rs
@@ -1,7 +1,134 @@
 use crate::error::ReconcileError;
+use lazy_static::lazy_static;
 use reqwest::{RequestBuilder, Response, StatusCode};
+use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
+use std::collections::HashMap;
 use url::{PathSegmentsMut, Url};
+
+// NOTE: We need to amend this with every field we add
+const FIELD_MASK_WEBHOOK: &[&str] = &[
+    "base_url",
+    "downlink_ack",
+    "downlink_api_key",
+    "downlink_failed",
+    "downlink_nack",
+    "downlink_queue_invalidated",
+    "downlink_queued",
+    "downlink_sent",
+    "format",
+    "headers",
+    "ids",
+    "ids.application_ids",
+    "ids.application_ids.application_id",
+    "ids.webhook_id",
+    "join_accept",
+    "location_solved",
+    "service_data",
+    "uplink_message",
+    "uplink_message.path",
+];
+
+const FIELD_MASK_DEVICE_CORE: &[&str] = &[
+    "ids.dev_eui",
+    "ids.join_eui",
+    "name",
+    "description",
+    "attributes",
+    "join_server_address",
+    "network_server_address",
+    "application_server_address",
+];
+const FIELD_MASK_DEVICE_JS: &[&str] = &[
+    "network_server_address",
+    "application_server_address",
+    "ids.device_id",
+    "ids.dev_eui",
+    "ids.join_eui",
+    "network_server_kek_label",
+    "application_server_kek_label",
+    "application_server_id",
+    "net_id",
+    "root_keys.app_key.key",
+];
+const FIELD_MASK_DEVICE_AS: &[&str] = &["ids.device_id", "ids.dev_eui", "ids.join_eui"];
+const FIELD_MASK_DEVICE_NS: &[&str] = &[
+    "multicast",
+    "supports_join",
+    "lorawan_version",
+    "ids.device_id",
+    "ids.dev_eui",
+    "ids.join_eui",
+    "mac_settings.supports_32_bit_f_cnt",
+    "supports_class_c",
+    "supports_class_b",
+    "lorawan_phy_version",
+    "frequency_plan_id",
+];
+
+lazy_static! {
+    static ref FIELD_MASK_WEBHOOK_STRING: String = FIELD_MASK_WEBHOOK.join(",");
+    static ref FIELD_MASK_WEBHOOK_STR: &'static str = &FIELD_MASK_WEBHOOK_STRING;
+}
+
+#[derive(Clone, Debug)]
+pub struct Device {
+    pub ids: DeviceIds,
+    pub end_device: EndDevice,
+    pub ns_device: NsDevice,
+    pub js_device: JsDevice,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct DeviceIds {
+    pub device_id: String,
+    pub dev_eui: String,
+    pub join_eui: String,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct EndDevice {
+    pub name: String,
+    pub network_server_address: String,
+    pub application_server_address: String,
+    pub join_server_address: String,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct NsDevice {
+    pub multicast: bool,
+    pub supports_join: bool,
+    pub lorawan_version: String,
+    pub lorawan_phy_version: String,
+
+    pub mac_settings: HashMap<String, Value>,
+    pub supports_class_b: bool,
+    pub supports_class_c: bool,
+    pub frequency_plan: String,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct JsDevice {
+    pub network_server_address: String,
+    pub application_server_address: String,
+    pub join_server_address: String,
+
+    pub network_server_kek_label: String,
+    pub application_server_kek_label: String,
+    pub application_server_id: String,
+    pub net_id: Value,
+    pub root_keys: RootKeys,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct RootKeys {
+    pub app_key: Key,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Key {
+    pub key: String,
+}
 
 pub struct Client {
     client: reqwest::Client,
@@ -100,11 +227,17 @@ impl Client {
         if res.status().is_success() {
             let payload = res.text().await?;
             log::debug!("Response: {:#?}", payload);
-        } else {
-            Self::default_error(res.status(), res).await?;
-        }
 
-        Ok(())
+            Ok(())
+        } else {
+            match res.status() {
+                StatusCode::CONFLICT => Err(ReconcileError::permanent(format!(
+                    "An application with the name '{}' already exists in the TTN system. This may be another user claiming that name, or a previously created application which is still pending final deletion. You can still choose to use a different name.",
+                    app_id
+                ))),
+                _ => Self::default_error(res.status(), res).await,
+            }
+        }
     }
 
     async fn default_error<T>(code: StatusCode, res: Response) -> Result<T, ReconcileError> {
@@ -151,7 +284,7 @@ impl Client {
             path.extend(&["api", "v3", "applications", app_id]);
         })?;
 
-        let res = self.client.get(url).inject_token(&ctx).send().await?;
+        let res = self.client.delete(url).inject_token(&ctx).send().await?;
 
         match res.status() {
             StatusCode::OK | StatusCode::NOT_FOUND => Ok(()),
@@ -169,11 +302,8 @@ impl Client {
             path.extend(&["api", "v3", "as", "webhooks", app_id, webhook_id]);
         })?;
 
-        {
-            url
-                .query_pairs_mut()
-                .append_pair("field_mask", "headers,uplink_message,join_accept,downlink_ack,downlink_nack,downlink_sent,downlink_failed,downlink_queued,downlink_queue_invalidated,location_solved,service_data");
-        }
+        url.query_pairs_mut()
+            .append_pair("field_mask", &FIELD_MASK_WEBHOOK_STR);
 
         let res = self.client.get(url).inject_token(&ctx).send().await?;
 
@@ -193,6 +323,9 @@ impl Client {
         ctx: &Context,
     ) -> Result<(), ReconcileError> {
         let create = json!({
+            "field_mask": {
+                "paths": FIELD_MASK_WEBHOOK,
+            },
             "webhook": {
                 "ids": {
                     "webhook_id": webhook_id,
@@ -206,16 +339,9 @@ impl Client {
             },
         });
 
-        let mut url = Self::url(&ctx, |path| {
-            //path.extend(&["api", "v3", "as", "webhooks", app_id, webhook_id]);
+        let url = Self::url(&ctx, |path| {
             path.extend(&["api", "v3", "as", "webhooks", app_id]);
         })?;
-
-        /*
-                url
-                    .query_pairs_mut()
-                    .append_pair("field_mask", "headers,uplink_message,join_accept,downlink_ack,downlink_nack,downlink_sent,downlink_failed,downlink_queued,downlink_queue_invalidated,location_solved,service_data");
-        */
 
         log::debug!("New webhook: {}", create);
 
@@ -233,6 +359,204 @@ impl Client {
         } else {
             Self::default_error(res.status(), res).await?;
         }
+
+        Ok(())
+    }
+
+    pub async fn update_webhook(
+        &self,
+        app_id: &str,
+        webhook_id: &str,
+        endpoint_url: &Url,
+        auth: &str,
+        ctx: &Context,
+    ) -> Result<(), ReconcileError> {
+        let update = json!({
+            "field_mask": {
+                "paths": FIELD_MASK_WEBHOOK,
+            },
+            "webhook": {
+                "base_url": endpoint_url,
+                "format": "json",
+                "uplink_message": {},
+                "headers": {
+                    "Authorization": auth,
+                }
+            },
+        });
+
+        let url = Self::url(&ctx, |path| {
+            path.extend(&["api", "v3", "as", "webhooks", app_id, webhook_id]);
+        })?;
+
+        log::debug!("New webhook: {}", update);
+
+        let res = self
+            .client
+            .put(url)
+            .inject_token(&ctx)
+            .json(&update)
+            .send()
+            .await?;
+
+        if res.status().is_success() {
+            let payload = res.text().await?;
+            log::debug!("Response: {:#?}", payload);
+        } else {
+            Self::default_error(res.status(), res).await?;
+        }
+
+        Ok(())
+    }
+
+    pub async fn get_device(
+        &self,
+        app_id: &str,
+        device_id: &str,
+        ctx: &Context,
+    ) -> Result<Option<Value>, ReconcileError> {
+        let url = Self::url(&ctx, |path| {
+            path.extend(&["api", "v3", "applications", app_id, "devices", device_id]);
+        })?;
+
+        let res = self.client.get(url).inject_token(&ctx).send().await?;
+
+        match res.status() {
+            StatusCode::OK => Ok(Some(res.json().await?)),
+            StatusCode::NOT_FOUND => Ok(None),
+            code => Self::default_error(code, res).await,
+        }
+    }
+
+    pub async fn delete_device(
+        &self,
+        app_id: &str,
+        device_id: &str,
+        ctx: &Context,
+    ) -> Result<(), ReconcileError> {
+        let url = Self::url(&ctx, |path| {
+            path.extend(&["api", "v3", "applications", app_id, "devices", device_id]);
+        })?;
+
+        let res = self.client.delete(url).inject_token(&ctx).send().await?;
+
+        match res.status() {
+            StatusCode::OK | StatusCode::NOT_FOUND => Ok(()),
+            code => Self::default_error(code, res).await,
+        }
+    }
+
+    fn make_device_json<T: Serialize>(
+        ids: &DeviceIds,
+        v: &T,
+        paths: &[&str],
+    ) -> Result<Value, ReconcileError> {
+        let mut json = json!({
+            "end_device": serde_json::to_value(&v)?,
+            "field_mask": {
+                "paths": paths,
+            }
+        });
+
+        json["end_device"]["ids"] = serde_json::to_value(&ids)?;
+
+        Ok(json)
+    }
+
+    async fn put_device_json<T: Serialize>(
+        &self,
+        prefix: &str,
+        app_id: &str,
+        ids: &DeviceIds,
+        payload: &T,
+        paths: &[&str],
+        ctx: &Context,
+    ) -> Result<(), ReconcileError> {
+        let url = Self::url(&ctx, |path| {
+            path.extend(&[
+                "api",
+                "v3",
+                prefix,
+                "applications",
+                app_id,
+                "devices",
+                &ids.device_id,
+            ]);
+        })?;
+
+        let res = self
+            .client
+            .put(url)
+            .inject_token(&ctx)
+            .json(&Self::make_device_json(&ids, &payload, paths)?)
+            .send()
+            .await?;
+
+        match res.status() {
+            StatusCode::OK => Ok(()),
+            code => Self::default_error(code, res).await?,
+        }
+    }
+
+    pub async fn create_device(
+        &self,
+        app_id: &str,
+        device_id: &str,
+        device: Device,
+        ctx: &Context,
+    ) -> Result<(), ReconcileError> {
+        // core
+
+        let url = Self::url(&ctx, |path| {
+            path.extend(&["api", "v3", "applications", app_id, "devices", device_id]);
+        })?;
+
+        let res = self
+            .client
+            .post(url)
+            .inject_token(&ctx)
+            .json(&Self::make_device_json(
+                &device.ids,
+                &device.end_device,
+                FIELD_MASK_DEVICE_CORE,
+            )?)
+            .send()
+            .await?;
+
+        match res.status() {
+            StatusCode::OK => {}
+            code => Self::default_error(code, res).await?,
+        }
+
+        // NS
+
+        self.put_device_json(
+            "ns",
+            app_id,
+            &device.ids,
+            &device.ns_device,
+            FIELD_MASK_DEVICE_NS,
+            &ctx,
+        )
+        .await?;
+        self.put_device_json(
+            "js",
+            app_id,
+            &device.ids,
+            &device.js_device,
+            FIELD_MASK_DEVICE_JS,
+            &ctx,
+        )
+        .await?;
+        self.put_device_json(
+            "as",
+            app_id,
+            &device.ids,
+            &json!({}),
+            FIELD_MASK_DEVICE_AS,
+            &ctx,
+        )
+        .await?;
 
         Ok(())
     }
