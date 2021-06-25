7fba3c369398949b2f7c4d34017cf7bba676c6e2
Jens Reimann <jreimann@redhat.com>
2021-06-25 16:39:59 +0200

    Don't require jq

--- scripts/lib/tools.sh
+++ scripts/lib/tools.sh
@@ -8,7 +8,6 @@ function check_std_tools() {
 
     command -v 'kubectl' &>/dev/null || die "Missing the command 'kubectl'"
     command -v 'curl' &>/dev/null || die "Missing the command 'curl'"
-    command -v 'jq' &>/dev/null || die "Missing the command 'jq'"
     command -v 'sed' &>/dev/null || die "Missing the command 'sed'"
     command -v 'docker' &>/dev/null || command -v 'podman' &>/dev/null || die "Missing the command 'docker' or 'podman'"
     command -v 'helm' &>/dev/null || die "Missing the command 'helm'"
