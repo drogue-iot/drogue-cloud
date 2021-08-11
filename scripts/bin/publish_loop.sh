e4faa79e796d1bdf151c7cd41ee3849bcb2eb6d9
Jens Reimann <jreimann@redhat.com>
2021-08-11 16:13:09 +0200

    Allow tweaking more settings for the publish loop

--- scripts/bin/publish_loop.sh
+++ scripts/bin/publish_loop.sh
@@ -7,11 +7,15 @@
 : "${ENDPOINT:="https://http-endpoint-drogue-iot.apps.my.cluster"}"
 : "${CHANNEL:="foo"}"
 : "${MODEL_ID:="io.drogue.demo:FirstTestDevice:1.0.0"}"
-: "${APP:=app_id}"
+: "${APP:=example-app}"
 : "${DEVICE:=device_id}"
 : "${PASS:=foobar}"
+: "${OFFSET:=10}"
+: "${RANGE:=10}"
+: "${FACTOR:=0.02}"
 
 while true; do
-    http -v --auth "${DEVICE}@${APP}:${PASS}" ${CERT:+--verify $CERT} POST "${ENDPOINT}/v1/${CHANNEL}" "model_id==$MODEL_ID" temp:="$(printf "%f" "$(echo "s ( $(date +%s) * 0.02 ) * 10 + 10" | bc -l)")"
+    # shellcheck disable=SC2086
+    http -v --auth "${DEVICE}@${APP}:${PASS}" ${CERT:+--verify $CERT} POST "${ENDPOINT}/v1/${CHANNEL}" "model_id==$MODEL_ID" temp:="$(printf "%f" "$(echo "s ( $(date +%s) * $FACTOR ) * $RANGE + $OFFSET" | bc -l)")" "$@"
     sleep 5
 done
