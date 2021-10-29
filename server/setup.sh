294e6bbbeaac490aef8737f0ef847317494fad84
Ulf Lilleengen <lulf@redhat.com>
2021-10-29 17:25:51 +0200

    Adjust

--- server/setup.sh
+++ server/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Migrating database"
-podman  run -e DATABASE_URL=postgres://admin:admin123456@localhost:5432/drogue -e POSTGRES_DB=drogue -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=admin123456 --net=host ghcr.io/drogue-iot/database-migration:latest
-
 echo "Setting up keycloak"
+docker exec server_keycloak_1 bash -c '/opt/jboss/keycloak/bin/kcadm.sh config credentials --server http://localhost:8080/auth --realm master --user admin --password admin123456'
+docker cp ./drogue-client.json server_keycloak_1:/tmp/drogue-client.json
+docker exec server_keycloak_1 bash -c '/opt/jboss/keycloak/bin/kcadm.sh create clients -f /tmp/drogue-client.json' 
