50a6e13c0a098e040240a5deac8a5c6663d88855
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2021-11-16 12:19:01 +0100

    Revert "Debug"
    This reverts commit e65aee71828b06094c02aca1c2f5c27e866c779e.

--- server/src/main.rs
+++ server/src/main.rs
@@ -588,7 +588,6 @@ fn main() {
             let t = token_config;
             let bind_addr = s.console.clone().into();
             let registry = registry.clone();
-            let user_auth = user_auth.clone();
             threads.push(std::thread::spawn(move || {
                 let config = drogue_cloud_console_backend::Config {
                     oauth: o,
@@ -601,7 +600,14 @@ fn main() {
                     console_token_config: Some(t.clone()),
                     disable_account_url: false,
                     scopes: "openid profile email".into(),
-                    user_auth,
+                    user_auth: Some(UserAuthClientConfig {
+                        token_config: Some(t),
+                        url: Url::parse(&format!(
+                            "http://{}:{}",
+                            s.user_auth.host, s.user_auth.port
+                        ))
+                        .unwrap(),
+                    }),
                 };
                 actix_rt::System::with_tokio_rt(|| {
                     tokio::runtime::Builder::new_current_thread()
