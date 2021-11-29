df5c5573dd71d3c1a67b1735ef8bc76c9f2f08a7
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2021-11-29 14:40:21 +0100

    Remove bad commit

--- coap-endpoint/src/main.rs
+++ coap-endpoint/src/main.rs
@@ -1,8 +1,7 @@
 use drogue_cloud_coap_endpoint::{run, Config};
 use drogue_cloud_service_common::app;
-use tokio::signal;
 
 #[actix_web::main]
 async fn main() -> anyhow::Result<()> {
-    futures::try_join!(app!(), signal::ctrl_c().await)
+    app!();
 }
