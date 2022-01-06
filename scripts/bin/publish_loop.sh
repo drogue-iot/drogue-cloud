3cc16a787b6bed518f30fd357302026d008ba1a1
Dejan Bosanac <dejan@sensatic.net>
2022-01-06 15:45:42 +0100

    Support mqtt in publish_loop.sh script

--- scripts/bin/publish_loop.sh
+++ scripts/bin/publish_loop.sh
@@ -4,7 +4,10 @@
 # Publish a "temperature" every 5 seconds
 #
 
+: "${TYPE:="http"}"
 : "${ENDPOINT:="https://http-endpoint-drogue-iot.apps.my.cluster"}"
+: "${PORT:=30001}"
+: "${SLEEP:=5}"
 : "${CHANNEL:="foo"}"
 : "${MODEL_ID:="io.drogue.demo:FirstTestDevice:1.0.0"}"
 : "${APP:=example-app}"
@@ -16,6 +19,13 @@
 
 while true; do
     # shellcheck disable=SC2086
-    http -v --auth "${DEVICE}@${APP}:${PASS}" ${CERT:+--verify $CERT} POST "${ENDPOINT}/v1/${CHANNEL}" "model_id==$MODEL_ID" temp:="$(printf "%f" "$(echo "s ( $(date +%s) * $FACTOR ) * $RANGE + $OFFSET" | bc -l)")" "$@"
-    sleep 5
+    case ${TYPE} in
+    "http")
+        http -v --auth "${DEVICE}@${APP}:${PASS}" ${CERT:+--verify $CERT} POST "${ENDPOINT}/v1/${CHANNEL}" "model_id==$MODEL_ID" temp:="$(printf "%f" "$(echo "s ( $(date +%s) * $FACTOR ) * $RANGE + $OFFSET" | bc -l)")" "$@"
+        ;;
+    "mqtt")
+        mqtt pub -v -h ${ENDPOINT} -p ${PORT} -u ${DEVICE}@${APP} -pw ${PASS} -s ${CERT:+--cafile $CERT} -t temp -m "$(printf "%f" "$(echo "s ( $(date +%s) * $FACTOR ) * $RANGE + $OFFSET" | bc -l)")"
+        ;;
+    esac
+    sleep ${SLEEP}
 done
