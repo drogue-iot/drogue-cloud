abee15377e186d0f11a0227d2723c083ff6c0484
Jens Reimann <jreimann@redhat.com>
2022-03-21 10:51:38 +0100

    build: allow overriding with local values

--- console-frontend/trunk.d/build.sh
+++ console-frontend/trunk.d/build.sh
@@ -7,4 +7,10 @@ set -e
 if [[ "$TRUNK_PROFILE" == "debug" ]]; then
     echo "Copy dev folder to staging: $TRUNK_STAGING_DIR"
     cp -a dev/endpoints "$TRUNK_STAGING_DIR"
+
+    # check if we have a local file, which will override
+    if [[ -f "$TRUNK_STAGING_DIR/endpoints/backend.local.json" ]]; then
+        echo "Override with local settings..."
+        cp "$TRUNK_STAGING_DIR/endpoints/backend.local.json" "$TRUNK_STAGING_DIR/endpoints/backend.json"
+    fi
 fi
