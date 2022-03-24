ef8bcf991ef6a28495d47fe978b174b6bc575df9
Jens Reimann <jreimann@redhat.com>
2022-03-24 18:24:34 +0100

    build: rename module to support `make -C`

--- topic-strimzi-operator/src/main.rs
+++ topic-strimzi-operator/src/main.rs
@@ -1,5 +1,5 @@
 use drogue_cloud_service_common::app;
-use drogue_cloud_topic_operator::{run, Config};
+use drogue_cloud_topic_strimzi_operator::{run, Config};
 
 #[actix::main]
 async fn main() -> anyhow::Result<()> {
