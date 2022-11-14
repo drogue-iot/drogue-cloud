d55b9c4033478406a58ec29bb25061ad709e575b
jbtrystram <jbtrystram@redhat.com>
2022-11-14 14:00:17 +0100

    [feat] Add a migration script to update devices credentials

---
+++ credentials-migrate.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/sh
+
+if [ $# -lt 1 ]
+then
+  echo "Application ID must be supplied. Usage: ./credentials-migrate.sh <app-id>"
+  exit 1
+fi
+APPLICATION=$1
+
+which jq > /dev/null && which drg > /dev/null
+if [ $? -ne 0 ]
+then
+  echo "jq and drg not found. These tools are mandatory to run this script"
+  exit 1
+fi
+
+# test for a valid drg config
+CONFIG=$(drg config show --active -o json | jq .name)
+if [ $? -ne 0 ]
+then
+  echo "drg is not logged in. Set up drg before continuing with: drg login <url>"
+  exit 1
+fi
+
+echo "Using drg config $CONFIG"
+
+# get a list of device | keep only column 1 | remove header
+DEVICES_ID=$(drg get devices --app "$APPLICATION" | awk '{print $1}' | tail -n +2)
+
+for dev in $DEVICES_ID
+do
+  DATA=$(drg get device "$dev" -o json --app "$APPLICATION")
+  hascreds=$(echo "$DATA" | jq '.spec | has("credentials")')
+
+  if [ "$hascreds" = "true" ]
+  then
+    DATA=$(echo "$DATA" | jq '.spec.authentication = .spec.credentials | del(.spec.credentials)')
+    echo "$DATA" | drg apply -f -
+  else
+      echo "No credentials field for device $dev"
+  fi
+
+done
